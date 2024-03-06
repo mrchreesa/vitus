b816cb820b51eaf3c2e3281ab0fd0be10da134b9
Kristian Rahnev <mrchreesa@gmail.com>
2024-03-06 17:11:28 +0000

    adding text

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 // Not in Use
-import Counselling from "@/components/Counselling";
-import { getLocale } from "next-intl/server";
+// import Counselling from "@/components/Counselling";
+// import { getLocale } from "next-intl/server";
 
 export default function Home() {
-  const locale = getLocale();
-  console.log(locale);
-  return <Counselling />;
+  // const locale = getLocale();
+  // console.log(locale);
+  // return <Counselling />;
+  return null;
 }
