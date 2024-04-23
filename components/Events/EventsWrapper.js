828eb7d0b364bdc7fa321e7f855254eb072a4043
Kristian Rahnev <mrchreesa@gmail.com>
2024-04-23 15:43:01 +0100

    events

--- components/Events/EventsWrapper.js
+++ components/Events/EventsWrapper.js
@@ -11,6 +11,13 @@ const EventsWrapper = async ({ locale }) => {
   } else {
     filteredEvents = events.filter((event) => event.language.en === true);
   }
+  if (!filteredEvents.length) {
+    return (
+      <div className="font-mont flex h-screen w-full justify-center items-center text-green">
+        No upcoming events
+      </div>
+    );
+  }
   return <EventsComponent events={filteredEvents} />;
 };
 
