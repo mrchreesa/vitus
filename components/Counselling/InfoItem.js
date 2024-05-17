cb95f88838464f5a5e3a8bae7f68f4731d6e56fc
Kristian Rahnev <mrchreesa@gmail.com>
2024-05-17 10:53:51 +0100

    font issue

--- components/Counselling/InfoItem.js
+++ components/Counselling/InfoItem.js
@@ -1,23 +1,37 @@
 import React from "react";
 
 const InfoItem = ({ title, text, image, imageAlt, reverse }) => {
-  const words = text.split(" "); // Split the string into an array of words by spaces
-  const firstTwoWords = words.slice(0, 2).join(" "); // Join the first two words into a string
-  const remainingWords = words.slice(2).join(" ");
+  // const words = text.split(" "); // Split the string into an array of words by spaces
+  // const firstTwoWords = words.slice(0, 2).join(" "); // Join the first two words into a string
+  // const remainingWords = words.slice(2).join(" ");
+  function highlightMarkedWords(description) {
+    // Regular expression to find words between the special symbols (*)
+    const regex = /\*([^*]+)\*/g;
+    return description.replace(
+      regex,
+      '<span class="font-bold italic">$1</span>'
+    );
+  }
+
+  const highlightedDescription = highlightMarkedWords(text);
+
   return (
     <div
       className={` flex flex-col ${
         reverse ? "md:flex-row-reverse md:mr-10" : "md:flex-row md:pl-10"
       } w-full mt-8 md:mt-16`}
     >
-      <div className="w-full md:flex-1 flex items-center font-mont text-green">
+      <div className="w-full md:flex-1  flex items-center font-mont text-green">
         <div className="p-8 md:p-12 lg:p-16">
           <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 md:mb-6">
             {title}
           </h2>
           <p className="text-base md:text-lg mb-4 ">
-            <span className="font-bold mr-1">{firstTwoWords}</span>
-            {remainingWords}
+            <span
+              dangerouslySetInnerHTML={{ __html: highlightedDescription }}
+            ></span>
+            {/* <span className="font-bold mr-1">{firstTwoWords}</span>
+            {remainingWords} */}
           </p>
         </div>
       </div>
