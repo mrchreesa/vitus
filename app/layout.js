bfdb0de29c3f26f733ef4724ba0427902d83b3d7
Kristian Rahnev <mrchreesa@gmail.com>
2024-07-19 11:47:22 +0100

    meta

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,17 @@
 import "./globals.css";
 
 
+export const metadata = {
+  title: "Centre Vitus",
+  description: "Providing individual and family counselling",
+  openGraph: {
+    images: "/opengraph-image.png",
+  
 
+    
+  },
+
+};
 
 export default function RootLayout({ children }) {
   return children;
