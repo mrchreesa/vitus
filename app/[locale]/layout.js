424a91adb1dbb520d546914fb36ad412a07d8dc3
Kristian Rahnev <mrchreesa@gmail.com>
2024-07-19 11:03:25 +0100

    meta

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -60,32 +60,66 @@ export default function LocaleLayout({ children, params: { locale } }) {
   };
 
   return (
-    <html lang={locale} className={`${mont.variable} font-sans ${typo.variable} font-sans`}>
+    <html
+      lang={locale}
+      className={`${mont.variable} font-sans ${typo.variable} font-sans`}
+    >
       <Head>
         <title>{metadata.title}</title>
         <meta charSet="UTF-8" />
         <meta name="description" content={metadata.description} />
         <link rel="icon" href={metadata.icons.icon} />
 
+        <meta
+          name="description"
+          content="Providing individual and family counselling"
+        />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemprop="name" content="Centre Vitus" />
+        <meta
+          itemprop="description"
+          content="Providing individual and family counselling"
+        />
+        <meta
+          itemprop="image"
+          content="https://i.postimg.cc/3yjphgG2/vitus-en.jpg"
+        />
+
         {/* <!-- Facebook Meta Tags --> */}
-        <meta property="og:url" content="https://www.centrevitus.com" />
+        <meta property="og:url" content="https://www.centrevitus.com/bg" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.openGraph.images} />
+        <meta property="og:title" content="Centre Vitus" />
+        <meta
+          property="og:description"
+          content="Providing individual and family counselling"
+        />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/3yjphgG2/vitus-en.jpg"
+        />
 
         {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="centrevitus.com" />
-        <meta property="twitter:url" content="https://www.centrevitus.com" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.openGraph.images} />
+        <meta name="twitter:title" content="Centre Vitus" />
+        <meta
+          name="twitter:description"
+          content="Providing individual and family counselling"
+        />
+        <meta
+          name="twitter:image"
+          content="https://i.postimg.cc/3yjphgG2/vitus-en.jpg"
+        />
 
         <link rel="preload" href="/maze.jpeg" as="image" />
       </Head>
       <body>
-        <NavBar locale={locale} navigation={navigation} enquiry={enquiry} services={services} />
+        <NavBar
+          locale={locale}
+          navigation={navigation}
+          enquiry={enquiry}
+          services={services}
+        />
         {children}
         <Footer navigation={navigation} locale={locale} />
       </body>
