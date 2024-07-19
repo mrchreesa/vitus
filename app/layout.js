5205c22f4a364b6cdcd1b3948ffa543beae444ad
Kristian Rahnev <mrchreesa@gmail.com>
2024-07-19 09:27:24 +0100

    metadata

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
 import "./globals.css";
 
+
 export const metadata = {
   title: "Centre Vitus",
   description: "Providing individual and family counselling",
   openGraph: {
-    images: '/opengraph-image.jpg',
+    images: 'https://i.postimg.cc/DzHcq6yp/vitus-en.jpg',
   },
   icons: {
     icon: "/vitus-en.jpg",
